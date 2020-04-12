3b89147d63cdb62b7c02bd82f71a0058d8d7e0c8
swapnil31121995 <swapnilphapale@yahoo.com>
2020-04-12 18:14:48 +0530

    adding

--- pathon.py
+++ pathon.py
@@ -7,4 +7,9 @@ This is check for push
 =======
 adding for the pull check
 this check for push check in the file
->>>>>>> d917e16cc82cf656b41dfab39db1d8edb974ac04
+>>>>>>> d917e16cc82cf656b41dfab39db1d8edb974ac0'
+
+
+
+
+ddingsdsdsdlmsdklsmdklsndklsndklsnkln4
