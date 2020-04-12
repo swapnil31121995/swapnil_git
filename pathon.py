b943156d61a2a56f906d314c919e2f7623334b93
swapnil31121995 <swapnilphapale@yahoo.com>
2020-04-12 18:28:02 +0530

    rollback

--- pathon.py
+++ pathon.py
@@ -1,15 +1,4 @@
 print("welcome to swapnil house")
 print ("This is only for testing purpose")
-<<<<<<< HEAD
-print ("add this in commit")
-print ("add this in scond  commit")
-This is check for push
-=======
 adding for the pull check
 this check for push check in the file
->>>>>>> d917e16cc82cf656b41dfab39db1d8edb974ac0'
-
-
-
-
-ddingsdsdsdlmsdklsmdklsndklsndklsnkln4
