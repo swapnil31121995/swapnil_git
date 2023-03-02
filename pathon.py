63b65c888bb8e802c44dc7c71270239b5812c4ee
swapnil31121995 <swapnilphapale@yahoo.com>
2023-03-02 08:03:19 +0530

    to add file .py

--- pathon.py
+++ pathon.py
@@ -1,4 +1,5 @@
 print("welcome to swapnil house")
 print ("This is only for testing purpose")
-adding for the pull check
-this check for push check in the file
+
+
+print("Swapnil is leraning the code")
